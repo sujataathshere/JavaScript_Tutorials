d8c14c4ef0d049e33882f8506b32c8e9aa1c1a6c
Sujata Athshere <sujataathshere@gmail.com>
2024-12-03 12:53:29 +0530

    Learned let variable

---
+++ Variables/Let.js
@@ -0,0 +1,11 @@
+let fullName = "Sujata Athshere"
+let age = 25
+let id = 987
+let salary = 15.5
+
+console.log(fullName);
+
+let schoolName = "Swami Muktanand Vidyayala, Yeola"
+schoolName = "Janata Vidyayala, Yeola"
+
+console.log(schoolName);
--- Variables/Var.js
+++ Variables/Var.js
@@ -4,3 +4,8 @@ var id = 987
 var salary = 15.5
 
 console.log(fullName);
+
+var schoolName = "Swami Muktanand Vidyayala, Yeola"
+var schoolName = "Janata Vidyayala, Yeola"
+
+console.log(schoolName);
