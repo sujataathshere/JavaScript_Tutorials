33191eb5c929546cfe08da74bbfd861dc5141549
Sujata Athshere <sujataathshere@gmail.com>
2024-12-08 12:12:12 +0530

    Solved Question 2 from practice set7

---
+++ Practice_Set/Set7/Set7_Q2.js
@@ -0,0 +1,22 @@
+const vowelCount = (str) => {
+    let count = 0;
+    for (let char of str) {
+      if (
+        char === "a" ||
+        char === "e" ||
+        char === "i" ||
+        char === "o" ||
+        char === "u" ||
+        char === "A" ||
+        char === "E" ||
+        char === "I" ||
+        char === "O" ||
+        char === "U"
+      ) {
+        count++;
+      }
+    }
+    return count;
+}
+  
+console.log(vowelCount("I Love JavaScript"));
