a13358948a4b7ef6f5c5a236aa1d6ee29311a425
Sujata Athshere <sujataathshere@gmail.com>
2024-12-10 14:28:38 +0530

    Learned how to change css styling using node in js

--- Chapter7/Attribute/node.js
+++ Chapter7/Attribute/node.js
@@ -4,6 +4,12 @@ let divNode = document.querySelector("div");
 divNode.style.backgroundColor = "skyblue";
 
 let pNode = document.getElementsByClassName("para");
-console.log(pNode);
+console.log("P tage node :",pNode);
 pNode[0].style.color = "lightgreen";
 pNode[1].style.fontFamily = "Courier New";
+
+let spanNode = document.getElementById("sp");
+console.log("Span tag node :",spanNode);
+spanNode.style.backgroundColor = "yellow";
+spanNode.style.color = "red";
+spanNode.style.fontSize = "18px";
